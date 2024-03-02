dd0e51ee00ecd2e1345bae004acb63150d42e03c
bens <mangga.asem@gmail.com>
2024-03-02 17:52:48 +0700

    push 24

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+const envPath = path.resolve(__dirname, '../.env');
+
 
 const app = express();
-dotenv.config();
+dotenv.config({ path: envPath });
 const connectDB = async () => {
     try {
         mongoose.connect(process.env.MONGO_URI)
@@ -33,22 +35,22 @@ app.use("/5R2I/users", userRoute)
 app.use("/5R2I/todo", todoRoute)
 
 
-app.use("*", (req, res) => {
-    res.json("PAGE NOT FOUND")
-})
+// app.use("*", (req, res) => {
+//     res.json("PAGE NOT FOUND")
+// })
 
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static(path.join(__dirname, "./client/build")));
+    app.use(express.static(path.join(__dirname, "../client/build")));
 
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
     });
 }
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT || 3001, () => {
-    console.log("server RUNNING on Port", PORT);
+app.listen(PORT, () => {
+    console.log("server RUNNING on Port :", PORT);
 });
